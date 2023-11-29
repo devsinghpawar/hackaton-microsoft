250b6ac4747a7bb76ae5ee6429280791005e45bd
Mario <mceseahurtado00@mylangara.ca>
2023-11-28 17:12:40 -0800

    feat: Importing all functionality & Adding basic styling

---
+++ microsoft-hackaton-project/components/SpeechToTextComponent.js
@@ -0,0 +1,113 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+import { saveAs } from "file-saver";
+
+// styles
+import styles from "./Components.module.scss";
+
+const SPEECH_KEY = process.env.NEXT_PUBLIC_SPEECH_KEY;
+const SPEECH_REGION = process.env.NEXT_PUBLIC_SPEECH_REGION;
+
+const SpeechToTextComponent = () => {
+  const [isListening, setIsListening] = useState(false);
+  const speechConfig = useRef(null);
+  const audioConfig = useRef(null);
+  const recognizer = useRef(null);
+  const [recognizingTranscript, setRecognizingTranscript] = useState("");
+  const [recognizedTranscript, setRecognizedTranscript] = useState("");
+
+  useEffect(() => {
+    speechConfig.current = sdk.SpeechConfig.fromSubscription(
+      SPEECH_KEY,
+      SPEECH_REGION
+    );
+    speechConfig.current.speechRecognitionLanguage = "en-US";
+
+    audioConfig.current = sdk.AudioConfig.fromDefaultMicrophoneInput();
+    recognizer.current = new sdk.SpeechRecognizer(
+      speechConfig.current,
+      audioConfig.current
+    );
+
+    recognizer.current.recognized = (s, e) => {
+      const result = e.result;
+      if (result.reason === sdk.ResultReason.RecognizedSpeech) {
+        const timestamp = new Date().toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        const newText = `[${timestamp}] ${result.text}\n`;
+        setRecognizedTranscript((prevTranscript) => prevTranscript + newText);
+      }
+    };
+
+    recognizer.current.recognizing = (s, e) => {
+      const result = e.result;
+      if (result.reason === sdk.ResultReason.RecognizingSpeech) {
+        setRecognizingTranscript(result.text);
+      }
+    };
+
+    recognizer.current.startContinuousRecognitionAsync(() => {
+      setIsListening(true);
+    });
+
+    return () => {
+      recognizer.current.stopContinuousRecognitionAsync(() => {
+        setIsListening(false);
+      });
+    };
+  }, []);
+
+  const pauseListening = () => {
+    setIsListening(false);
+    recognizer.current.stopContinuousRecognitionAsync();
+  };
+
+  const resumeListening = () => {
+    if (!isListening) {
+      setIsListening(true);
+      recognizer.current.startContinuousRecognitionAsync();
+    }
+  };
+
+  const stopListening = () => {
+    setIsListening(false);
+    recognizer.current.stopContinuousRecognitionAsync(() => {
+      saveTranscript(recognizedTranscript);
+    });
+  };
+
+  const saveTranscript = (transcript) => {
+    const blob = new Blob([transcript], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "transcript.txt");
+  };
+
+  return (
+    <>
+      <div className={styles.btnContainer}>
+        <button className={styles.btn} onClick={resumeListening}>
+          Resume Listening
+        </button>
+        <button className={styles.btn} onClick={pauseListening}>
+          Pause Listening
+        </button>
+        <button className={styles.btn} onClick={stopListening}>
+          Stop Listening
+        </button>
+      </div>
+      <div>
+        <div className={styles.transcriptText}>
+          Recognizing Transcript: {recognizingTranscript}
+        </div>
+        <div className={styles.transcriptText}>
+          Recognized Transcript: {recognizedTranscript}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SpeechToTextComponent;
